75e203d5b931efb87589e84198a09115c90c572f
Isaac Rossi <isaacjohnrossi@gmail.com>
2023-01-23 11:20:12 +1100

    fixed the behaviour of the timer counting below zero

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const short = document.querySelector(".buttons__short")
 const countdown = document.querySelector(".countdown")
 
-function startTimer () {
+function startTimer() {
   let duration = this.dataset.time;
-  const myInterval = setInterval( function () { //30
+  const myInterval = setInterval(function () { 
     duration --
     let minutes = Math.floor(duration / 60)
-    let unflooredMinutes = duration / 60
     let extraSeconds = duration % 60;
+    if (duration == 0) {
+      clearInterval(myInterval)
+      countdown.innerHTML = minutes + ':' + extraSeconds
+    }
     extraSeconds < 10 ? extraSeconds = '0' + extraSeconds : extraSeconds
-    if (unflooredMinutes === 0) {
-      myInterval(clearInterval)
-    } 
     countdown.innerHTML = minutes + ':' + extraSeconds
+    console.log(countdown.innerHTML)
   }, 10);
 }
 
 short.addEventListener("click", startTimer)
+
+
 
 // total seconds in 5 minutes is 300 seconds
 // divide 300 seconds by 60 gives us 5 minutes
