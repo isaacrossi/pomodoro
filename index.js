59ca0d5793d2ff43dde9877ad05c28d9a096eb9e
Isaac Rossi <isaacjohnrossi@gmail.com>
2023-01-25 17:45:36 +1100

    added in start and pause functionality

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ const start = document.querySelector(".start-button")
 
 const buttons = document.querySelectorAll('div.buttons button')
 
-let duration;
+let isPaused = true;
+
+let duration = focus.dataset.time
+let myInterval;
 const audio = new Audio('alarm.mp3')
 
 function startTimer() {
+
+  isPaused = !isPaused;
+
+  isPaused ? start.innerHTML = "start" : start.innerHTML = "pause"
 
   buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -18,30 +25,45 @@ function startTimer() {
     })
   });
 
-  const myInterval = setInterval(function () { 
-    duration --
+  clearInterval(myInterval)
+
+  myInterval = setInterval( function () {
+
+    if (!isPaused) duration --
+
     let minutes = Math.floor(duration / 60)
     let extraSeconds = duration % 60;
     extraSeconds < 10 ? extraSeconds = '0' + extraSeconds : extraSeconds
     countdown.innerHTML = minutes + ':' + extraSeconds
+
     if (duration <= 0 && short.classList.contains('active')) {
+      isPaused = true;
       audio.play()
       clearInterval(myInterval)
       countdown.innerHTML = short.dataset.time / 60 + ':00'
       duration = short.dataset.time
+      start.innerHTML = "Start"
     } else if (duration <= 0 && focus.classList.contains('active')) {
+      isPaused = true;
       audio.play()
       clearInterval(myInterval)
       countdown.innerHTML = focus.dataset.time / 60 + ':00'
       duration = focus.dataset.time
+      start.innerHTML = "Start"
     } else if (duration <= 0 && long.classList.contains('active')) {
+      isPaused = true;
       audio.play()
       clearInterval(myInterval)
       countdown.innerHTML = long.dataset.time / 60 + ':00'
       duration = long.dataset.time
+      start.innerHTML = "Start"
     }
-  }, 10);
+
+    console.log(duration)
+  }, 1000);
+
 }
+
 
 start.addEventListener("click", startTimer)
 
@@ -51,6 +73,8 @@ short.addEventListener("click", (e) => {
   short.classList.add('active')
   focus.classList.remove('active')
   long.classList.remove('active')
+  isPaused = true;
+  start.innerHTML = "Start"
 })
 
 
@@ -60,6 +84,8 @@ focus.addEventListener("click", (e) => {
   focus.classList.add('active')
   short.classList.remove('active')
   long.classList.remove('active')
+  isPaused = true;
+  start.innerHTML = "Start"
 })
 
 long.addEventListener("click", (e) => {
@@ -68,5 +94,7 @@ long.addEventListener("click", (e) => {
   long.classList.add('active')
   focus.classList.remove('active')
   short.classList.remove('active')
+  isPaused = true;
+  start.innerHTML = "Start"
 })
 
